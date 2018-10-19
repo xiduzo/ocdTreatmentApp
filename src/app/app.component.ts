df997bdd013d75d48cc17c26d8fea79a5707f51b
sander boer <mail@sanderboer.nl>
2018-10-19 10:05:01 +0200

    Demo time
    Removed login for demo purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { Storage } from '@ionic/storage';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { OnboardingPage } from '../pages/onboarding/onboarding';
-import { LoginPage } from '../pages/login/login';
+//import { OnboardingPage } from '../pages/onboarding/onboarding';
+//import { LoginPage } from '../pages/login/login';
 
 import { AuthService, UserService } from '../lib/services';
 
@@ -21,7 +21,7 @@ import { defaultLanguage, availableLanguages, sysOptions } from '../lib/constant
   templateUrl: 'app.html'
 })
 export class MyApp {
-  private rootPage:any = LoginPage; // Always start the app with the LoginPage to be sure
+  private rootPage:any = TabsPage; // Always start the app with the LoginPage to be sure
 
   constructor(
     protected appCtrl: App,
@@ -35,60 +35,74 @@ export class MyApp {
     protected globalization: Globalization,
     protected screenOrientation: ScreenOrientation
     ) {
-    platform.ready().then(() => {
-      // We only let the users use the app in portrait, bc its fucked up in landscape (sorry not sorry)
-      if(platform.platforms().find(platform => { return platform === 'core' })) screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT);
 
-      // Set the language for the app
-      this.setLanguage();
+      platform.ready().then(() => {
+        // We only let the users use the app in portrait, bc its fucked up in landscape (sorry not sorry)
+        //if(platform.platforms().find(platform => { return platform === 'core' })) screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT);
 
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+        // Set the language for the app
+        this.setLanguage();
 
-      // Uncomment to start with fresh user
-      // storage.clear();
-
-      // Get the user
-      storage.get('user').then((user) => {
-        if(user) { userService.setUser(user); }
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
       });
 
-      // Set rootPage based on storage 'jwtToken' and 'onboardingCompleted'
-      storage.get('jwtToken')
-      .then((val) => {
-        const localToken = val;
-
-        if(localToken) {
-          // Try to verify the token
-          authService.verifyJwtToken({token: localToken})
-          .then((resp) => {
-            // Token is verified --> refresh the token
-            authService.refreshJwtToken({token: localToken})
-            .then((resp:any) => {
-              // Set the token for the app
-              authService.setLocalToken(resp.token);
-              // Token is refreshed!
-              // Check if the user allready did the onboarding
-              storage.get('onboardingCompleted')
-              .then((val) => {
-                // Based on the 'onboardingCompleted' we guide the user to the next page
-                val === true ? this.appCtrl.getRootNav().push(TabsPage) : this.appCtrl.getRootNav().push(OnboardingPage);
-              })
-              // Something went wrong getting the 'onboardingCompleted'
-              .catch((err) => { console.log(err); });
-            })
-            // Error refreshing token
-            .catch((err) => { console.log(err); });
-          })
-          // Token expired
-          .catch((err) => { console.log(err); });
-        }
-      })
-      // Something went wrong getting the 'jwtToken'
-      .catch((err) => { console.log(err); });
-    });
+    // platform.ready().then(() => {
+    //   // We only let the users use the app in portrait, bc its fucked up in landscape (sorry not sorry)
+    //   //if(platform.platforms().find(platform => { return platform === 'core' })) screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT);
+    //
+    //   // Set the language for the app
+    //   this.setLanguage();
+    //
+    //   // Okay, so the platform is ready and our plugins are available.
+    //   // Here you can do any higher level native things you might need.
+    //   statusBar.styleDefault();
+    //   splashScreen.hide();
+    //
+    //   // Uncomment to start with fresh user
+    //   // storage.clear();
+    //
+    //   // Get the user
+    //   storage.get('user').then((user) => {
+    //     if(user) { userService.setUser(user); }
+    //   });
+    //
+    //   // Set rootPage based on storage 'jwtToken' and 'onboardingCompleted'
+    //   storage.get('jwtToken')
+    //   .then((val) => {
+    //     const localToken = val;
+    //
+    //     if(localToken) {
+    //       // Try to verify the token
+    //       authService.verifyJwtToken({token: localToken})
+    //       .then((resp) => {
+    //         // Token is verified --> refresh the token
+    //         authService.refreshJwtToken({token: localToken})
+    //         .then((resp:any) => {
+    //           // Set the token for the app
+    //           authService.setLocalToken(resp.token);
+    //           // Token is refreshed!
+    //           // Check if the user allready did the onboarding
+    //           storage.get('onboardingCompleted')
+    //           .then((val) => {
+    //             // Based on the 'onboardingCompleted' we guide the user to the next page
+    //             val === true ? this.appCtrl.getRootNav().push(TabsPage) : this.appCtrl.getRootNav().push(OnboardingPage);
+    //           })
+    //           // Something went wrong getting the 'onboardingCompleted'
+    //           .catch((err) => { console.log(err); });
+    //         })
+    //         // Error refreshing token
+    //         .catch((err) => { console.log(err); });
+    //       })
+    //       // Token expired
+    //       .catch((err) => { console.log(err); });
+    //     }
+    //   })
+    //   // Something went wrong getting the 'jwtToken'
+    //   .catch((err) => { console.log(err); });
+    // });
   }
 
   setLanguage() {
