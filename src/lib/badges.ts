988d76b6d6a68cb10d17c68fd0a5a14c27d667bf
sander boer <mail@sanderboer.nl>
2018-10-21 23:37:40 +0200

    Badge system
    Added a badge factory to make new badges

--- src/lib/badges.ts
+++ src/lib/badges.ts
@@ -1,62 +1,44 @@
-export const badges = [
-  {
-    verbose: "GO GETTER",
-    stages: [
-      {
-        amountNeeded: 10,
-        description: 'Lorem ipsum dolor sit amet',
-        image: ''
-      },
-      {
-        amountNeeded: 100,
-        description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
-        image: ''
-      },
-      {
-        amountNeeded: 1000,
-        description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        image: ''
-      }
-    ],
-
-  }
-];
-
 import { Storage } from '@ionic/storage';
-import { Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-// @Injectable()
 export class Badge {
 
-  @Inject(Storage) storage:Storage;
-
-  // private name:string;
-  // private description:string;
-  // private progress:number;
-  // private points:number = 0;
-
   constructor(
-    // private name,
-    // private description,
-    // private progress,
-    // private points,
+    private name: string,
+    private description: string,
+    private stages: Array<number>,
+    private points: number,
+    private triggers: Array<string>,
+    private storage: Storage
   ) {
     // console.log(this.storage, Storage);
-    // Storage.get('fearLadder').then((val) => {
-    //   console.log(val);
-    // });
-    // console.log(this.storage);
+    this.storage.get('fearLadder').then((val) => {
+      console.log(val);
+    });
   }
 
   getProgress() {
-    // this.storage.get(this.name).then((val) => {
-    //   console.log(val);
-    //   return val;
-    // });
   }
 
   setPoints(value) {
     console.log(value, this.storage);
     // this.storage.set(this.name, value);
+  }
+}
+
+@Injectable()
+export class BadgeFactory {
+
+  constructor(private storage:Storage) {
+  }
+
+  public createBadge(
+    name: string,
+    description:string,
+    stages: Array<number>,
+    points: number,
+    triggers: Array<string>
+  ) {
+    return new Badge(name, description, stages, points, triggers, this.storage);
   }
 }
