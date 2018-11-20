1e6bb61fb701b99e0a3cb7f5e90ddc34037ac1af
sander boer <mail@sanderboer.nl>
2018-11-20 20:55:51 +0100

    Refectored exercise lib

--- src/lib/exercise.ts
+++ src/lib/exercise.ts
@@ -1,57 +1,88 @@
 import { UUID } from 'angular2-uuid';
 
 export class Step {
-  private id:string = UUID.UUID();
-  private fearRating:number = 1;
-  private triggers:Array<Trigger> = [];
-  private fear:Fear;
+  constructor(
+    public id:string = UUID.UUID(),
+    public fearRating:number = 1,
+    public triggers:Array<Trigger> = [],
+    public fear:Fear = null
+  ) {
+    this.id = id;
+    this.fearRating = fearRating;
+    this.triggers = triggers;
+    this.fear = fear;
+  }
 
-  constructor() {
+  addEmptyTriggers() {
     this.triggers.push(new Trigger('INTENSITY_OBSESSIVE_THOUGHTS'));
     this.triggers.push(new Trigger('INTENSITY_COMPULSIVE_BEHAVIOUR'));
-    this.fear = new Fear();
   }
 }
 
 export class Trigger {
-  private range:number = 0;
-  private explanation:string = null;
-  private enabled:boolean = false;
-
   constructor(
-    private verbose:string
+    public verbose:string,
+    public range:number = 0,
+    public explanation:string = null,
+    public enabled:boolean = false
   ) {
     this.verbose = verbose;
+    this.range = range;
+    this.explanation = explanation;
+    this.enabled = enabled;
   }
 }
 
 export class Fear {
-  private completion:number = 0;
-  private situation:string = null;
-  private without:string = null;
-
-  constructor() {
-
+  constructor(
+    public completion:number = 0,
+    public situation:string = null,
+    public without:string = null
+  ) {
+    this.completion = completion;
+    this.situation = situation;
+    this.without = without;
   }
 }
 
 export class Exercise {
-  private id:string = UUID.UUID();
-  private afterMood:Mood;
-  private beforeMood:Mood;
-  private step:Step;
-  private start:string; // moment
-  private end:string; // moment
-  private erp:Erp;
+  constructor(
+    public id:string = UUID.UUID(),
+    public afterMood:Mood,
+    public beforeMood:Mood,
+    public step:Step,
+    public start:string, // moment
+    public end:string, // moment
+    public erp:Erp
+  ) {
+    this.id = id;
+    this.afterMood = afterMood;
+    this.beforeMood = beforeMood;
+    this.step = step;
+    this.start = start;
+    this.end = end;
+    this.erp = erp;
+  }
 }
 
 export class Mood {
-  private mood:number;
-  private explanation:string;
+  constructor(
+    public mood:number,
+    public explanation:string
+  ) {
+    this.mood = mood;
+    this.explanation = explanation;
+  }
 }
 
 export class Erp {
-  private gaveInToCompulsion:boolean;
-  private start:string; // moment
-  private end:string; // moment
+  constructor(
+    public gaveInToCompulsion:boolean,
+    public start:string, // moment
+    public end:string // moment
+  ) {
+    this.gaveInToCompulsion = gaveInToCompulsion;
+    this.start = start;
+    this.end = end;
+  }
 }
