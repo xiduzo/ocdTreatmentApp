590367027ac9703b71f87307db746f663b86bf46
sander boer <mail@sanderboer.nl>
2018-12-16 18:54:46 +0100

    Make use of real badges

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -7,20 +7,23 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 import { AuthService, UserService } from '../../lib/services';
 
 import { BadgeModal } from '../badge/badge';
-import { Badge } from '../../lib/badges/Badge';
-import { streakBadge } from '../../lib/badges/templates/streak';
+import { Badge, BadgeFactory } from '../../lib/badges/Badge';
+import { STREAK_BADGE } from '../../lib/badges/templates/streak';
 
 import { SettingsPage } from '../settings/settings';
 
+
 @Component({
   selector: 'page-profile',
-  templateUrl: 'profile.html'
+  templateUrl: 'profile.html',
+  providers: [BadgeFactory]
 })
 export class ProfilePage {
 
-  public personalGoal:string;
-  public editGoal:boolean = false;
-  public view:string = 'achievements';
+  public personalGoal: string;
+  public editGoal: boolean = false;
+  public view: string = 'achievements';
+  public badges: Array<Badge> = [];
 
   constructor(
     private appCtrl: App,
@@ -30,18 +33,19 @@ export class ProfilePage {
     private storage: Storage,
     private emailComposer: EmailComposer,
     private notifications: LocalNotifications,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private badgeFctry: BadgeFactory,
   ) {
+    const badge = this.badgeFctry.createBadge(STREAK_BADGE);
+    this.badges.push(badge);
   }
 
   ionViewDidLoad() {
     this.storage.get('personalGoal').then((goal) => {
-      if(!goal) return;
+      if (!goal) return;
 
       this.personalGoal = goal;
     });
-
-    console.log(Badge, streakBadge);
   }
 
   openSettings() {
@@ -50,16 +54,16 @@ export class ProfilePage {
   }
 
   sendFearLadder() {
-    this.emailComposer.isAvailable().then((available: boolean) =>{
+    this.emailComposer.isAvailable().then((available: boolean) => {
       // TODO https://forum.ionicframework.com/t/how-to-create-a-json-file-ionic-3/105939/3
       alert(available);
       let email = {
         to: 'mail@sanderboer.nl',
         attachments: [
-        'file://img/logo.png',
-        'res://icon.png',
-        'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-        'file://README.pdf'
+          'file://img/logo.png',
+          'res://icon.png',
+          'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+          'file://README.pdf'
         ],
         subject: 'Test mail ionic',
         body: 'Whattttttt',
