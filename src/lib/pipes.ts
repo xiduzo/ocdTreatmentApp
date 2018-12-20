509627847b99111074c9e14b441ecc38ee6ed4a0
sander boer <mail@sanderboer.nl>
2018-12-21 00:02:40 +0100

    commented unused code

--- src/lib/pipes.ts
+++ src/lib/pipes.ts
@@ -72,14 +72,14 @@ export class msToTimePipe implements PipeTransform {
 }
 
 // http://embed.plnkr.co/Y3in02HYKneMNeotN6Nv/
-@Pipe({
-  name: 'truncate'
-})
-export class TruncatePipe {
-  transform(value: string, args: string[]): string {
-    let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
-    let trail = args.length > 1 ? args[1] : '...';
-
-    return value.length > limit ? value.substring(0, limit) + trail : value;
-  }
-}
+// @Pipe({
+//   name: 'truncate'
+// })
+// export class TruncatePipe {
+//   transform(value: string, args: string[]): string {
+//     let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
+//     let trail = args.length > 1 ? args[1] : '...';
+//
+//     return value.length > limit ? value.substring(0, limit) + trail : value;
+//   }
+// }
