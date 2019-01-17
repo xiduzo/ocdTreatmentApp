6460d3625c8498c6a32c3f3deca64fccf3ec36ae
sander boer <mail@sanderboer.nl>
2019-01-18 00:00:40 +0100

    Init rating calculation

--- src/pages/exercise/success/exercise.success.ts
+++ src/pages/exercise/success/exercise.success.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController } from 'ionic-angular';
 import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
+import { Storage } from '@ionic/storage';
 
 import { Exercise } from '../../../lib/Exercise';
 
@@ -21,6 +22,7 @@ export class ExerciseSuccessModal {
     private params: NavParams,
     public viewCtrl: ViewController,
     private nativePageTransitions: NativePageTransitions,
+    private storage: Storage,
   ) {
     this.nativePageTransitions.slide(this.options);
   }
@@ -28,9 +30,43 @@ export class ExerciseSuccessModal {
   ionViewWillEnter() {
     this.level = this.params.get('level');
     this.exercise = this.params.get('exercise');
+    this.calculateRating();
   }
 
   ionViewDidLoad() {
+  }
+
+  calculateRating() {
+    let points = 0;
+
+    // Mood diff
+    points += (this.exercise.beforeMood.mood - this.exercise.afterMood.mood) * 0.25;
+
+    // fearRating
+    points += this.exercise.step.fearRating * 5;
+
+    // triggers
+    this.exercise.step.triggers.forEach(trigger => {
+      points += trigger.range * -7.5;
+    });
+
+    // duration
+    points += this.exercise.getErpTimeDifference() / this.exercise.getTotalTimeDifference() * 25;
+
+    this.updateStepCompletion(this.exercise.step, points)
+  }
+
+  updateStepCompletion(exerciseStep, points) {
+    this.storage.get('fearLadder').then(fearLadder => {
+      try {
+        // TODO: update step in list view (maybe with an broadcast)
+        fearLadder.find(step => step.id === exerciseStep.id).fear.completion += points;
+      } catch (err) {
+        console.log(`err: ${err}`);
+      } finally {
+        this.storage.set('fearLadder', fearLadder);
+      }
+    });
   }
 
 
