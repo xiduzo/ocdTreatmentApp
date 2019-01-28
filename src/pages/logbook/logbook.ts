b4a0f7e15fb069663279eab1d35314a12030b20d
sander boer <mail@sanderboer.nl>
2019-01-28 09:24:43 +0100

    changed logbook style

--- src/pages/logbook/logbook.ts
+++ src/pages/logbook/logbook.ts
@@ -9,7 +9,7 @@ import { map } from '../../lib/helpers';
 })
 export class LogbookPage {
 
-  public exercises:any = [];
+  public exercises:Array<any> = [];
 
   constructor(
     private storage: Storage
@@ -24,16 +24,21 @@ export class LogbookPage {
     this.storage.get('exercises').then((exercises) => {
       if(!exercises) return;
       // Only show the exercises where a comment has been made
-      this.exercises = exercises.filter((exercise) => {
+      this.exercises = exercises
+      .sort((a,b) => { return b.start - a.start })
+      .map(exercise => {
+        console.log(exercise);
         // Map the moods to 1-5 scale
         if(exercise.beforeMood) exercise.beforeMood.mood = Math.round(map(exercise.beforeMood.mood, 0, 500, 1, 5));
         if(exercise.afterMood) exercise.afterMood.mood = Math.round(map(exercise.afterMood.mood, 0, 500, 1, 5));
 
+        return exercise;
         // If any explanation has been provided, return the exercise
-        if(exercise.beforeMood && exercise.beforeMood.explanation) return exercise;
-        if(exercise.afterMood && exercise.afterMood.explanation) return exercise;
-        if(exercise.step.triggers.find(trigger => { return trigger.explanation })) return exercise;
+        // if(exercise.beforeMood && exercise.beforeMood.explanation) return exercise;
+        // if(exercise.afterMood && exercise.afterMood.explanation) return exercise;
+        // if(exercise.step.triggers.find(trigger => { return trigger.explanation })) return exercise;
       });
+      console.log(this.exercises);
     });
   }
 
