8429b566e4c3d21b0d3085af6dad3ed24a066bfb
sander boer <mail@sanderboer.nl>
2019-02-12 15:55:42 +0100

    Fixed difference pipe

--- src/lib/pipes/difference.ts
+++ src/lib/pipes/difference.ts
@@ -1,14 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import moment from 'moment';
-
 @Pipe({ name: 'difference' })
 export class differencePipe implements PipeTransform {
   transform(base: number, compare: number, decimalPoint: number = 1): string {
-    if (!base || !compare) return false;
+    if (!base || !compare) return '0';
 
-    const difference = ((compare * 100 / base) - 100).toFixed(decimalPoint);
+    const diff:number = parseFloat(((compare * 100 / base) - 100).toFixed(decimalPoint));
 
-    return `${difference > 0 ? '+' : ''}${difference}`;
+    return `${diff > 0 ? '+' : ''}${diff}`;
   }
 }
