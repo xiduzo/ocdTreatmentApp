d00a1bcb79756f767502e6737b647b57fbce750e
sander boer <mail@sanderboer.nl>
2019-02-12 09:23:35 +0100

    Make use of the eventservice

--- src/pages/progress/progress.ts
+++ src/pages/progress/progress.ts
@@ -6,6 +6,10 @@ import regression from 'regression';
 
 import { mapRange } from '../../lib/helpers';
 
+import { Exercise } from '../../lib/Exercise';
+
+import { EventsService } from 'angular-event-service';
+
 @Component({
   selector: 'progress-home',
   templateUrl: 'progress.html'
@@ -23,8 +27,12 @@ export class ProgressPage {
   public thisWeeksExercises:Array<Exercise> = [];
   public previousWeeksExercises:Array<Exercise> = [];
 
+  private lastAddedExercise: Exercise;
+  private hasAddedNewExercises: boolean = false;
+
   constructor(
-    private storage: Storage
+    private storage: Storage,
+    private eventService: EventsService
   ) {
     this._chartOptions = {
       chart: {
@@ -92,17 +100,78 @@ export class ProgressPage {
           name: 'Observations',
           data: [],
           color: '#7D8CA9',
-          // enableMouseTracking: false,
+          enableMouseTracking: false,
           marker: { symbol: 'round', radius: 6 }
         },
+        // {
+        //   name: 'Callibration',
+        //   data: [1,2,3,4,5].map(x => [x,x])
+        // }
       ],
       credits: { href: null, text: '' }
     }
   }
 
   ionViewDidEnter() {
-    // Every time the view entered, try to update the graph
+    // Every time the view entered, check if we need to recalculate the regression line
+    if(this.hasAddedNewExercises) {
+      this.addRegressionToGraph();
+      this.hasAddedNewExercises = false;
+    }
+  }
+
+  ionViewWillEnter() {
+    this.eventService.on('exercise_update', this.exerciseUpdate.bind(this));
+  }
+
+  ionViewWillLoad() {
     this.getExercises();
+  }
+
+  ionViewWillLeave() {
+    this.eventService.destroyListener('exercise_update', this.exerciseUpdate);
+  }
+
+  exerciseUpdate(exercise: Exercise) {
+    const localExercise = this.exercises.find(currExercise => currExercise.id === exercise.id);
+
+    [exercise.beforeMood, exercise.afterMood].forEach(mood => {
+      if(mood.mood !== null) mood.mappedMood = mood.getMappedMood();
+    });
+
+    if(!localExercise) {
+      this.exercises.push(exercise);
+    } else {
+      this.exercises[this.exercises.indexOf(localExercise)] = exercise;
+    }
+
+    // Check if we need to add the exercise to the graph or anything else
+    this.checkToAddExercise(exercise, localExercise);
+  }
+
+  checkToAddExercise(exercise: Exercise, exerciseExists: boolean) {
+    if(moment(this.endWeek).week() === moment(exercise.start).week()) {
+      if(exerciseExists) {
+        this.thisWeeksExercises[this.thisWeeksExercises.indexOf(exercise)] = exercise;
+      } else {
+        this.thisWeeksExercises.push(exercise);
+      }
+
+      // Check if we have a before and after mood
+      if(exercise.beforeMood.mood !== null && exercise.afterMood.mood !== null) {
+        //check if the point has not been added to the graph
+        if(this.lastAddedExercise.id !== exercise.id) {
+          // Add a point to the graph
+          this.addExercisePointToGraph(exercise);
+        }
+
+        this.lastAddedExercise = exercise;
+        this.hasAddedNewExercises = true; // To recalculate the new regression line on enter
+      }
+    }
+
+    // CHECK: i think we can not add something to the previous week stats
+    // As we have limited the user to go forward in time on the week selector
   }
 
   // Use this for editing the chart dynamicly later on
@@ -110,62 +179,79 @@ export class ProgressPage {
     this.chart = chart;
   }
 
+  addExercisePointToGraph(exercise: Exercise) {
+    this.chart.series[1].addPoint([
+      mapRange(exercise.afterMood.mood, 0, 500, 1, 5),
+      mapRange(exercise.beforeMood.mood, 0, 500, 1, 5)
+    ]);
+
+    this.lastAddedExercise = exercise;
+  }
+
   getExercises() {
-    this.exercises = [];
     this.storage.get('exercises').then((exercises) => {
       if(!exercises) return;
 
       this.exercises = exercises;
 
       // Now update the graph
-      this.addPointsToGraph(exercises, moment(this.endWeek).week());
+      this.addExercisesToGraph(this.exercises, moment(this.endWeek).week());
     });
   }
 
-  clearPoints() {
-    // Clear the observations
-    const observations = this.chart.series[1].data.length;
-    for(let i = 0; i < observations; i++) {
-      this.chart.series[1].removePoint(0);
+  clearSeries(serieNumber: number) {
+    const amount = this.chart.series[serieNumber].data.length;
+    for(let i = 0; i < amount; i++) {
+      this.chart.series[serieNumber].removePoint(0);
     }
-
-    // Clear the regression line
-    const regression = this.chart.series[this.chart.series.length-2].data.length;
-    for(let i = 0; i < regression; i++) {
-      this.chart.series[0].removePoint(0);
-    }
-
-    // Update the graph again
-    this.addPointsToGraph(this.exercises, moment(this.endWeek).week());
   }
 
-  addPointsToGraph(exercises:any, weekNumber:number) {
-    this.thisWeeksExercises = exercises
-    // Get the exercises for this week
-    .filter((exercise) => { return moment(exercise.start).week() === weekNumber})
-    .forEach(exercise => {
-      // Check if there is a before and aftermood registered
-      if((exercise.hasOwnProperty('beforeMood') && exercise.hasOwnProperty('afterMood'))) {
-        this.chart.series[1].addPoint([
-          mapRange(exercise.afterMood.mood, 0, 500, 1, 5),
-          mapRange(exercise.beforeMood.mood, 0, 500, 1, 5)
-        ]);
-      }
-    });
+  clearRegression() {
+    // Remove the regression points
+    this.clearSeries(0);
+  }
+
+  clearExercises() {
+    // Remove the exercises
+    this.clearSeries(1);
+  }
+
+  addRegressionToGraph() {
+    // First clear the old regression line
+    this.clearRegression();
 
     // Update the regression line
     // https://github.com/Tom-Alexander/regression-js
     regression
-    .polynomial(this.chart.series[1].data
-    // .linear(this.chart.series[this.chart.series.length-1].data
-    // Only need the mapped x and y values
-    .map(point => { return [point.x, point.y]})).points
-    // Sort in order to for a smooth line
+    .polynomial(
+      this.chart.series[1].data
+      // Only need the mapped x and y values
+      .map(point => [point.x, point.y])
+    ).points
+    // Sort the points in order to for a smooth line
     .sort()
     .forEach(point => {
       // Add the points to the graph
       this.chart.series[0].addPoint(point);
     });
+  }
+
+  addExercisesToGraph(exercises:Array<Exercise>, weekNumber:number) {
+    // First clear the old exercises
+    this.clearExercises();
+
+    exercises
+    // Get the exercises for this week
+    .filter((exercise) => moment(exercise.start).week() === weekNumber)
+    .forEach(exercise => {
+      // Check if there is a before- and aftermood
+      if(exercise.beforeMood.mood !== null && exercise.afterMood.mood !== null) {
+        this.addExercisePointToGraph(exercise);
+      }
+    });
+
+    // Add the regression line again
+    this.addRegressionToGraph();
 
     // Update the stats
     this.updateStats(exercises, weekNumber);
@@ -174,11 +260,11 @@ export class ProgressPage {
   updateStats(exercises:any, weekNumber:number) {
     // Get the exercises for this week
     this.thisWeeksExercises = exercises
-    .filter((exercise) => { return moment(exercise.start).week() === weekNumber});
+    .filter((exercise) => moment(exercise.start).week() === weekNumber);
 
     // Get the exercises for previous week
     this.previousWeeksExercises = exercises
-    .filter((exercise) => { return moment(exercise.start).week() === weekNumber - 1});
+    .filter((exercise) => moment(exercise.start).week() === weekNumber - 1);
   }
 
   changeWeek(direction:number) {
@@ -189,7 +275,8 @@ export class ProgressPage {
     // Cant go further than this week
     this.canSelectNextWeek = !Boolean(moment(this.endWeek).week() === moment(moment.now()).week());
 
-    this.clearPoints();
+    // Update the graph again
+    this.addExercisesToGraph(this.exercises, moment(this.endWeek).week());
   }
 
 }
