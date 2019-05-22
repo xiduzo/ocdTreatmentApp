56cf7f118763c6970e5f8e42ce99d8e919baaa53
sander boer <mail@sanderboer.nl>
2019-05-22 16:17:56 +0200

    added analytics

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,12 +4,17 @@ import { AppModule } from './app.module';
 
 import awsmobile from '../aws-exports';
 
-import Auth from '@aws-amplify/auth';
-import Storage from '@aws-amplify/storage';
-import Analytics from '@aws-amplify/Analytics';
+// import Auth from '@aws-amplify/auth';
+// import Storage from '@aws-amplify/storage';
+import Amplify, { Analytics } from 'aws-amplify';
+Amplify.configure(awsmobile);
 
-Auth.configure(awsmobile);
-Storage.configure(awsmobile);
-Analytics.configure(awsmobile);
+Analytics.record({ name: 'anEvent' });
+
+// Auth.configure(awsmobile);
+// Storage.configure(awsmobile);
+// Analytics.configure(awsmobile);
+
+// Analytics.configure({ disabled: true });
 
 platformBrowserDynamic().bootstrapModule(AppModule);
