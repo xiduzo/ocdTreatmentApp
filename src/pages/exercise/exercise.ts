9726fb0b72c81ffaa423196362de4ccbf57a2b92
sander boer <mail@sanderboer.nl>
2019-05-30 15:28:08 +0200

    Used redux state for level list
    it works so smooth!

--- src/pages/exercise/exercise.ts
+++ src/pages/exercise/exercise.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { App, ModalController } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
 
 import { ExerciseListModal } from '@/modals/exercise/list/exercise.list';
 
@@ -8,93 +7,45 @@ import { ToastController } from 'ionic-angular';
 
 import { FearladderModal } from '@/modals/fearladder/fearladder';
 
-import { EventsService } from 'angular-event-service';
-
-import { Level } from '@/lib/Level';
-import { Step } from '@/lib/Exercise';
-
 import { TranslateService } from 'ng2-translate/ng2-translate';
+
+import { FEAR_COMPLETION_POSITIVE_LIMIT } from '@/lib/constants';
+
+import { select } from '@angular-redux/store';
+import { Observable } from 'rxjs/Observable';
+import { IFearLadderState } from '@/stores/fearLadder/fearLadder.reducer';
+import { IStep } from '@/stores/exercise/exercise.model';
 
 @Component({
   selector: 'page-exercise',
   templateUrl: 'exercise.html'
 })
 export class ExercisePage {
-  public levels: Array<Level>;
+  @select() readonly fearLadder$: Observable<IFearLadderState>;
 
   constructor(
     public appCtrl: App,
-    private storage: Storage,
     public toastCtrl: ToastController,
     private modalCtrl: ModalController,
-    private eventService: EventsService,
     private translate: TranslateService
   ) {}
 
-  ionViewWillLoad() {
-    this.getExercises();
-    this.eventService.on(
-      'new_level_completion',
-      this.newLevelCompletion.bind(this)
+  getCompletion(level: IStep[]): number {
+    return (
+      level
+        // Map to array of completion percentages
+        .map(
+          (step: IStep): number =>
+            // Use constant to calculate completion of step
+            (step.fear.completion * 100) / FEAR_COMPLETION_POSITIVE_LIMIT
+        )
+        // Combine all the percentages into one number
+        .reduce(
+          (previousValue: number, currentValue: number): number =>
+            currentValue + previousValue,
+          0 // <-- Starting count of reduce
+        ) / level.length // Divide by amount of steps we have
     );
-    this.eventService.on('changed_fearladder', this.newFearladder.bind(this));
-  }
-
-  ionViewWillUnload() {
-    this.eventService.destroyListener(
-      'new_level_completion',
-      this.newLevelCompletion
-    );
-    this.eventService.destroyListener('changed_fearladder', this.newFearladder);
-  }
-
-  newLevelCompletion(level: Level) {
-    const localLevel = this.levels.find(
-      currLevel => currLevel.number === level.number
-    );
-
-    if (localLevel) {
-      this.levels[this.levels.indexOf(localLevel)].completion =
-        level.completion;
-      this.levels[this.levels.indexOf(localLevel)].isLevelDone();
-    }
-  }
-
-  newFearladder(fearladder: Array<Step>) {
-    this.getExercises();
-  }
-
-  getExercises() {
-    this.storage.get('fearLadder').then(fearLadder => {
-      if (!fearLadder) return (this.levels = []);
-      this.levels = [1, 2, 3, 4, 5, 6, 7, 8].map(
-        level => new Level({ number: level })
-      );
-
-      // TODO: fix this ugly code, it could be done faster I think
-      this.levels.forEach(level => (level.steps = []));
-      fearLadder.forEach((step: Step) => {
-        this.levels
-          .find(level => level.number === step.fearRating)
-          .steps.push(new Step(step));
-      });
-
-      // We don't need to see the levels which has no steps
-      this.levels = this.levels.filter(level => level.steps.length > 0);
-
-      this.setLevelsMonsterAndCompletion();
-    });
-  }
-
-  setLevelsMonsterAndCompletion() {
-    this.levels.forEach((level: Level) => {
-      level.calculateCompletion();
-      level.isLevelDone();
-      level.monster = `assets/imgs/monsters/monster-0${level.number}.svg`;
-      level.monster_sized = `assets/imgs/monsters/monster-0${
-        level.number
-      }_sized.svg`;
-    });
   }
 
   addFearsAndCompulsions() {
@@ -103,7 +54,7 @@ export class ExercisePage {
     });
 
     modal.onDidDismiss(data => {
-      this.setLevelsMonsterAndCompletion();
+      // this.setLevelsMonsterAndCompletion();
 
       this.translate
         .get('MESSAGE_CHANGE_FEAR_LADDER')
@@ -122,12 +73,12 @@ export class ExercisePage {
     modal.present();
   }
 
-  goToLevel(level: Level): void {
+  goToLevel(level: IStep[]): void {
     // Don't need to go there if there are no exercises
-    if (!level.steps.length) return;
+    if (!level.length) return;
 
-    // Also dont need to go there if the level is done
-    if (level.done) return;
+    // Also don't need to go there if the level is done
+    // if (level.done) return;
 
     this.appCtrl.getRootNav().push(ExerciseListModal, {
       level: level
