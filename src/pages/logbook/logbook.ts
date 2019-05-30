a6480816046e6db10723b37de66f70eb2bed2662
sander boer <mail@sanderboer.nl>
2019-05-30 22:32:49 +0200

    removed demo action

--- src/pages/logbook/logbook.ts
+++ src/pages/logbook/logbook.ts
@@ -5,7 +5,6 @@ import { Exercise } from '@/lib/Exercise';
 import { select } from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
 import { IExerciseState } from '@/stores/exercise/exercise.reducer';
-import { ExerciseActions } from '@/stores/exercise/exercise.action';
 
 @Component({
   selector: 'logbook-page',
@@ -14,13 +13,7 @@ import { ExerciseActions } from '@/stores/exercise/exercise.action';
 export class LogbookPage {
   @select() readonly exercises$: Observable<IExerciseState>;
 
-  constructor(
-    private exerciseActions: ExerciseActions // private store: NgRedux<IAppState>
-  ) {
-    setTimeout(() => {
-      this.exerciseActions.addExercise(new Exercise());
-    }, 2000);
-  }
+  constructor() {}
 
   toggleExerciseContent(exercise: Exercise) {
     exercise['openedContent'] = !exercise['openedContent'];
