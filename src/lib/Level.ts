cc6be1e12b6fe261e882fcdf86a0d98fb427e355
sander boer <mail@sanderboer.nl>
2019-05-30 22:29:28 +0200

    Added some functionality in the level lib

--- src/lib/Level.ts
+++ src/lib/Level.ts
@@ -1,6 +1,7 @@
 import { Step } from '@/lib/Exercise';
 import { UUID } from 'angular2-uuid';
 import { FEAR_COMPLETION_POSITIVE_LIMIT } from './constants';
+import { IStep } from '@/stores/exercise/exercise.model';
 
 export class Level {
   public id: string;
@@ -39,4 +40,22 @@ export class Level {
       (stepsCompleted.length * 100) / this.steps.length
     );
   }
+}
+
+export function getLevelCompletion(level: IStep[]): number {
+  return (
+    level
+      // Map to array of completion percentages
+      .map(
+        (step: IStep): number =>
+          // Use constant to calculate completion of step
+          (step.fear.completion * 100) / FEAR_COMPLETION_POSITIVE_LIMIT
+      )
+      // Combine all the percentages into one number
+      .reduce(
+        (previousValue: number, currentValue: number): number =>
+          currentValue + previousValue,
+        0 // <-- Starting count of reduce
+      ) / level.length // Divide by amount of steps we have
+  );
 }
