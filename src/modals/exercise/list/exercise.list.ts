3eaa7712c817782cf14122191dd897727d9584b4
sander boer <mail@sanderboer.nl>
2019-05-30 22:28:39 +0200

    Make use of redux in the exercise list page

--- src/modals/exercise/list/exercise.list.ts
+++ src/modals/exercise/list/exercise.list.ts
@@ -1,7 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
-import { Storage } from '@ionic/storage';
+import { Component } from '@angular/core';
 
-import { App, Content, NavParams, ModalController } from 'ionic-angular';
+import { App, NavParams, ModalController } from 'ionic-angular';
 
 import { ExerciseMoodModal } from '@/modals/exercise/mood/exercise.mood';
 
@@ -9,86 +8,54 @@ import { Exercise } from '@/lib/Exercise';
 
 import { FEAR_COMPLETION_POSITIVE_LIMIT } from '@/lib/constants';
 
-import { EventsService } from 'angular-event-service';
+import { IStep, IExercise } from '@/stores/exercise/exercise.model';
+
+import { getLevelCompletion } from '@/lib/Level';
+import { ExerciseActions } from '@/stores/exercise/exercise.action';
 
 @Component({
   selector: 'exercise-list-page',
   templateUrl: 'exercise.list.html'
 })
 export class ExerciseListModal {
-  @ViewChild(Content) content: Content;
-
-  private level: any;
-
-  public fearCompletionPositiveLimit = FEAR_COMPLETION_POSITIVE_LIMIT;
+  public FEAR_COMPLETION_POSITIVE_LIMIT: number = FEAR_COMPLETION_POSITIVE_LIMIT;
+  public level: IStep[];
 
   constructor(
     private params: NavParams,
     private modalCtrl: ModalController,
     private appCtrl: App,
-    private storage: Storage,
-    private eventService: EventsService
-  ) {}
-
-  ionViewWillEnter() {
+    private exerciseActions: ExerciseActions
+  ) {
     this.level = this.params.get('level');
-  }
-
-  ionViewWillLoad() {
-    this.eventService.on(
-      'completed_exercise',
-      this.exerciseCompleted.bind(this)
-    );
-  }
-
-  ionViewWillUnload() {
-    this.eventService.destroyListener(
-      'completed_exercise',
-      this.exerciseCompleted
-    );
-  }
-
-  exerciseCompleted(exercise: Exercise) {
-    try {
-      const step = this.level.steps.find(step => step.id === exercise.step.id);
-
-      if (!step) throw 'Step not found';
-
-      step.fear.completion = exercise.step.fear.completion;
-    } catch (err) {
-      console.log(`err ${err}`);
-    } finally {
-      this.level.calculateCompletion();
-      this.eventService.broadcast('new_level_completion', this.level);
-    }
+    console.log(this.level);
   }
 
   close() {
     this.appCtrl.getRootNav().pop();
   }
 
-  selectStep(step) {
+  getCompletion(): number {
+    return getLevelCompletion(this.level);
+  }
+
+  selectStep(step: IStep) {
+    // We can't start an exercise when you've completed it
     if (step.fear.completion >= FEAR_COMPLETION_POSITIVE_LIMIT) return;
 
-    this.storage.get('exercises').then(exercises => {
-      if (!exercises) exercises = []; // When it's the users' first time
-
-      const exercise = new Exercise({
-        start: new Date(),
-        step: step
-      });
-
-      exercises.push(exercise);
-
-      this.storage.set('exercises', exercises);
-
-      const exerciseMoodModal = this.modalCtrl.create(ExerciseMoodModal, {
-        level: this.level,
-        exercise: exercise,
-        before: true
-      });
-
-      exerciseMoodModal.present();
+    const exercise: IExercise = new Exercise({
+      start: new Date(),
+      step: step
     });
+
+    const exerciseMoodModal = this.modalCtrl.create(ExerciseMoodModal, {
+      level: this.level,
+      exercise: exercise,
+      before: true
+    });
+
+    this.exerciseActions.addExercise(exercise);
+
+    exerciseMoodModal.present();
   }
 }
