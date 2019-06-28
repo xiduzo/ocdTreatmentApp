5abcba1f0672a940315cba2082d7dc75bb829eea
sander boer <mail@sanderboer.nl>
2019-06-28 18:07:00 +0200

    made logbook more flexible
    and allready thinking of future for filtering

--- src/pages/logbook/logbook.ts
+++ src/pages/logbook/logbook.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Exercise } from '@lib/Exercise';
-
 import { select } from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
 import { IExerciseState } from '@stores/exercise/exercise.reducer';
+import { IExercise } from '@stores/exercise/exercise.model';
+
+import moment from 'moment';
 
 @Component({
   selector: 'logbook-page',
@@ -13,5 +14,18 @@ import { IExerciseState } from '@stores/exercise/exercise.reducer';
 export class LogbookPage {
   @select() readonly exercises$: Observable<IExerciseState>;
 
-  constructor() {}
+  public exercises: IExercise[] = [];
+  constructor() {
+    this.exercises$.subscribe((exerciseState: IExerciseState) => {
+      this.exercises = [...exerciseState.list];
+      this.filterAndSortExercises();
+    });
+  }
+
+  filterAndSortExercises = (): void => {
+    // TODO: add optional filtering
+    this.exercises.sort((a: IExercise, b: IExercise) => {
+      return moment(b.start).unix() - moment(a.start).unix();
+    });
+  };
 }
