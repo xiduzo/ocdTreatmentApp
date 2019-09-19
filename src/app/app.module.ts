748f8e4023ae7ae780912611ab6cf6524d6f7be1
sander boer <mail@sanderboer.nl>
2019-09-19 22:31:23 +0200

    auto formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,88 +1,84 @@
 /*------------------------------
   App
 ------------------------------*/
-import { MyApp } from './app.component';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component'
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular'
 
 /*------------------------------
   Angular
 ------------------------------*/
-import { NgModule, ErrorHandler, ApplicationRef } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { NgModule, ErrorHandler, ApplicationRef } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule, HttpClient } from '@angular/common/http'
 
 /*------------------------------
   Ionic
 ------------------------------*/
 // Native
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Globalization } from '@ionic-native/globalization';
-import { LocalNotifications } from '@ionic-native/local-notifications';
-import { ScreenOrientation } from '@ionic-native/screen-orientation';
-import { IonicStorageModule } from '@ionic/storage';
-import { NativePageTransitions } from '@ionic-native/native-page-transitions';
+import { StatusBar } from '@ionic-native/status-bar'
+import { SplashScreen } from '@ionic-native/splash-screen'
+import { Globalization } from '@ionic-native/globalization'
+import { LocalNotifications } from '@ionic-native/local-notifications'
+import { ScreenOrientation } from '@ionic-native/screen-orientation'
+import { IonicStorageModule } from '@ionic/storage'
+import { NativePageTransitions } from '@ionic-native/native-page-transitions'
 
 /*------------------------------
   AWS
 ------------------------------*/
 // Amplify
-import {
-  AmplifyAngularModule,
-  AmplifyService,
-  AmplifyModules
-} from 'aws-amplify-angular';
-import Auth from '@aws-amplify/auth';
+import { AmplifyAngularModule, AmplifyService, AmplifyModules } from 'aws-amplify-angular'
+import Auth from '@aws-amplify/auth'
 
 /*------------------------------
   Pages
 ------------------------------*/
 // Exercise
-import { ExercisePage } from '@pages/exercise/exercise';
-import { RatingPage } from '@pages/rating/rating';
+import { ExercisePage } from '@pages/exercise/exercise'
+import { RatingPage } from '@pages/rating/rating'
 // Auth
-import { LoginPage } from '@pages/auth/login/login';
-import { SignUpPage } from '@pages/auth/signup/signup';
-import { ConfirmCodePage } from '@pages/auth/confirmCode/confirmCode';
+import { LoginPage } from '@pages/auth/login/login'
+import { SignUpPage } from '@pages/auth/signup/signup'
+import { ConfirmCodePage } from '@pages/auth/confirmCode/confirmCode'
 // Onboarding
-import { OnboardingPage } from '@pages/onboarding/onboarding';
+import { OnboardingPage } from '@pages/onboarding/onboarding'
 // Logbook
-import { LogbookPage } from '@pages/logbook/logbook';
+import { LogbookPage } from '@pages/logbook/logbook'
 // Profile
-import { ProfilePage } from '@pages/profile/profile';
+import { ProfilePage } from '@pages/profile/profile'
 // Progress
-import { ProgressPage } from '@pages/progress/progress';
+import { ProgressPage } from '@pages/progress/progress'
 // Tabs (navigation)
-import { TabsPage } from '@pages/tabs/tabs';
+import { TabsPage } from '@pages/tabs/tabs'
 
 /*------------------------------
   Modals
 ------------------------------*/
 //Settings
-import { SettingsModal } from '@modals/settings/settings';
+import { SettingsModal } from '@modals/settings/settings'
 // Fear ladder
-import { FearLadderModal } from '@modals/fearLadder/fearLadder';
-import { FearLadderStepModal } from '@modals/fearLadder/step/fearLadder.step';
+import { FearLadderModal } from '@modals/fearLadder/fearLadder'
+import { FearLadderStepModal } from '@modals/fearLadder/step/fearLadder.step'
 // Exercise
-import { ExerciseMoodModal } from '@modals/exercise/mood/exercise.mood';
-import { ExerciseDuringModal } from '@modals/exercise/during/exercise.during';
-import { ExerciseTriggerModal } from '@modals/exercise/trigger/exercise.trigger';
-import { ExerciseSuccessModal } from '@modals/exercise/success/exercise.success';
-import { ExerciseListModal } from '@modals/exercise/list/exercise.list';
+import { ExerciseMoodModal } from '@modals/exercise/mood/exercise.mood'
+import { ExerciseDuringModal } from '@modals/exercise/during/exercise.during'
+import { ExerciseTriggerModal } from '@modals/exercise/trigger/exercise.trigger'
+import { ExerciseSuccessModal } from '@modals/exercise/success/exercise.success'
+import { ExerciseListModal } from '@modals/exercise/list/exercise.list'
 // Badge
-import { BadgeModal } from '@modals/badge/badge';
-import { BadgeEarnedModal } from '@modals/badgeEarned/badgeEarned';
+import { BadgeModal } from '@modals/badge/badge'
+import { BadgeEarnedModal } from '@modals/badgeEarned/badgeEarned'
 
 /*------------------------------
   Lib
 ------------------------------*/
 // pipes
-import { NgPipesModule } from 'ngx-pipes';
-import { msToTimePipe } from '@lib/pipes/msToTime';
-import { accumulateTimePipe } from '@lib/pipes/accumulateTime';
-import { differencePipe } from '@lib/pipes/difference';
-import { relativeTimePipe } from '@lib/pipes/relativeTime';
+import { NgPipesModule } from 'ngx-pipes'
+import { msToTimePipe } from '@lib/pipes/msToTime'
+import { accumulateTimePipe } from '@lib/pipes/accumulateTime'
+import { differencePipe } from '@lib/pipes/difference'
+import { relativeTimePipe } from '@lib/pipes/relativeTime'
 
 /*------------------------------
   Directives
@@ -91,81 +87,77 @@ import { relativeTimePipe } from '@lib/pipes/relativeTime';
 /*------------------------------
   Translation
 ------------------------------*/
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpModule } from '@angular/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { HttpModule } from '@angular/http'
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json')
 }
 
 /*------------------------------
   Datavisualizations
 ------------------------------*/
-import { ChartModule } from 'angular2-highcharts';
-import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
-import * as highcharts from 'highcharts/highcharts';
-import Highmore from 'highcharts/highcharts-more';
+import { ChartModule } from 'angular2-highcharts'
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService'
+import * as highcharts from 'highcharts/highcharts'
+import Highmore from 'highcharts/highcharts-more'
 
 /*------------------------------
   Redux
 ------------------------------*/
-import {
-  NgReduxModule,
-  NgRedux,
-  DevToolsExtension
-} from '@angular-redux/store';
+import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store'
 
 // Logger
-declare var require;
-var reduxLogger = require('redux-logger');
-import reduxLogger from 'redux-logger';
+declare var require
+var reduxLogger = require('redux-logger')
+import reduxLogger from 'redux-logger'
 
 // Thunk
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'
 
 // Actions
-import { ExerciseActions } from '@stores/exercise/exercise.action';
-import { FearLadderActions } from '@stores/fearLadder/fearLadder.action';
-import { BadgeActions } from '@stores/badge/badge.action';
+import { ExerciseActions } from '@stores/exercise/exercise.action'
+import { FearLadderActions } from '@stores/fearLadder/fearLadder.action'
+import { BadgeActions } from '@stores/badge/badge.action'
 
 // Reducers
-import { rootReducer, IAppState, INITIAL_STATE } from '@stores/reducer';
+import { rootReducer, IAppState, INITIAL_STATE } from '@stores/reducer'
 
 /*------------------------------
   Components
 ------------------------------*/
 // Mood indicator
-import { SpiritMoodIndicator } from '@components/moodIndicator/moodIndicator.component';
-import { SpiritMoodIndicatorMock } from '@components/moodIndicator/mock/moodIndicatorMock.component';
+import { SpiritMoodIndicator } from '@components/moodIndicator/moodIndicator.component'
+import { SpiritMoodIndicatorMock } from '@components/moodIndicator/mock/moodIndicatorMock.component'
 
 // Logbook item
-import { SpiritLogbookItem } from '@components/logbook/item/logbookItem.component';
-import { SpiritLogbookItemMock } from '@components/logbook/item/mock/logbookItemMock.component';
+import { SpiritLogbookItem } from '@components/logbook/item/logbookItem.component'
+import { SpiritLogbookItemMock } from '@components/logbook/item/mock/logbookItemMock.component'
 
 // Progress bar
-import { ProgressBarComponent } from '@components/progressBar/progressBar.component';
+import { ProgressBarComponent } from '@components/progressBar/progressBar.component'
 
 // Empty state
-import { EmptyStateComponent } from '@components/emptyState/emptyState.component';
+import { EmptyStateComponent } from '@components/emptyState/emptyState.component'
 
 // Exercise level
-import { ExerciseLevelComponent } from '@components/exerciseLevel/exerciseLevel.component';
+import { ExerciseLevelComponent } from '@components/exerciseLevel/exerciseLevel.component'
 
 // Exercise level step
-import { ExerciseLevelStepComponent } from '@components/exerciseLevelStep/exerciseLevelStep.component';
+import { ExerciseLevelStepComponent } from '@components/exerciseLevelStep/exerciseLevelStep.component'
 
 // Progress stat
-import { ProgressStatComponent } from '@components/progressStat/progressStat.component';
+import { ProgressStatComponent } from '@components/progressStat/progressStat.component'
 
 // Badge
-import { BadgeComponent } from '@components/badge/badge.component';
+import { BadgeComponent } from '@components/badge/badge.component'
 
 /*------------------------------
   Other
 ------------------------------*/
-import { RoundProgressModule } from 'angular-svg-round-progressbar';
-import { environment } from '@lib/environment';
+import { RoundProgressModule } from 'angular-svg-round-progressbar'
+import { environment } from '@lib/environment'
 
 @NgModule({
   declarations: [
@@ -207,7 +199,7 @@ import { environment } from '@lib/environment';
     ExerciseLevelComponent,
     ExerciseLevelStepComponent,
     ProgressStatComponent,
-    BadgeComponent
+    BadgeComponent,
   ],
   imports: [
     NgPipesModule,
@@ -216,11 +208,11 @@ import { environment } from '@lib/environment';
     IonicModule.forRoot(MyApp, {
       tabsPlacement: 'bottom',
       scrollPadding: false,
-      scrollAssist: false
+      scrollAssist: false,
     }),
     IonicStorageModule.forRoot({
       name: '__spiritDB',
-      driverOrder: ['indexeddb', 'sqlite', 'websql']
+      driverOrder: [ 'indexeddb', 'sqlite', 'websql' ],
     }),
     ChartModule,
     HttpClientModule,
@@ -231,12 +223,12 @@ import { environment } from '@lib/environment';
       loader: {
         provide: TranslateLoader,
         useFactory: createTranslateLoader,
-        deps: [HttpClient]
-      }
+        deps: [ HttpClient ],
+      },
     }),
-    NgReduxModule
+    NgReduxModule,
   ],
-  bootstrap: [IonicApp],
+  bootstrap: [ IonicApp ],
   entryComponents: [
     MyApp,
     // Pages
@@ -260,7 +252,7 @@ import { environment } from '@lib/environment';
     ExerciseDuringModal,
     ExerciseTriggerModal,
     ExerciseSuccessModal,
-    ExerciseListModal
+    ExerciseListModal,
   ],
   providers: [
     StatusBar,
@@ -278,29 +270,26 @@ import { environment } from '@lib/environment';
     {
       provide: DevToolsExtension,
       useClass: DevToolsExtension,
-      deps: [ApplicationRef, NgRedux]
+      deps: [ ApplicationRef, NgRedux ],
     },
     {
       provide: AmplifyService,
       useFactory: () => {
         return AmplifyModules({
-          Auth
-        });
-      }
-    }
-  ]
+          Auth,
+        })
+      },
+    },
+  ],
 })
 export class AppModule {
-  constructor(
-    private ngRedux: NgRedux<IAppState>,
-    private devTools: DevToolsExtension
-  ) {
+  constructor(private ngRedux: NgRedux<IAppState>, private devTools: DevToolsExtension) {
     this.ngRedux.configureStore(
       rootReducer,
       INITIAL_STATE,
-      environment.development ? [reduxLogger.createLogger(), thunk] : [thunk], // Skip logger for non dev builds
-      environment.development ? [this.devTools.enhancer()] : [] // Make sure we can build for devices
-    );
+      environment.development ? [ reduxLogger.createLogger(), thunk ] : [ thunk ], // Skip logger for non dev builds
+      environment.development ? [ this.devTools.enhancer() ] : [] // Make sure we can build for devices
+    )
   }
 }
 
@@ -309,12 +298,12 @@ export function highchartsFactory() {
   // Default options.
   highcharts.setOptions({
     global: {
-      useUTC: false
-    }
-  });
+      useUTC: false,
+    },
+  })
 
   // Initialize addons.
-  Highmore(highcharts);
+  Highmore(highcharts)
 
-  return highcharts;
+  return highcharts
 }
